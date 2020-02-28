69cc0ea7754f9339a916b00e95988a252003f0f3
zhengdongqi <1821260963@qq.com>
2020-02-28 14:37:18 +0800

    no

---
+++ WeiboDemo/WeiboDemo/PostCell.swift
@@ -0,0 +1,21 @@
+//
+//  PostCell.swift
+//  WeiboDemo
+//
+//  Created by 郑东琦 on 2020/2/19.
+//  Copyright © 2020 Nick. All rights reserved.
+//
+
+import SwiftUI
+
+struct PostCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostCell_Previews: PreviewProvider {
+    static var previews: some View {
+        PostCell()
+    }
+}
